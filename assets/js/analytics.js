2298caf0a5f4f011c4f8e6110d94974afa3d2585
Sages Services <jadenwhittle@hotmail.com>
2026-09-07 12:25:02 -0230

    Add files via upload

---
+++ assets/js/analytics.js
@@ -0,0 +1,8 @@
+(function () {
+  "use strict";
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () { window.dataLayer.push(arguments); };
+  window.gtag("js", new Date());
+  window.gtag("config", "G-Y0L9CV3XEL");
+})();
